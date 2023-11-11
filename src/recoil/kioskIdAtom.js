988e44261472474960e5633c191b14ac29cd98d0
seolbotdae <seolbotdae@gmail.com>
2023-11-11 23:21:34 +0900

    Feat/#11: 관리자가 키오스크를 시작시키는 기능 추가
    관리자가 키오스크 id를 사용해 키오스크를 시작시키는 기능 추가
    
    키오스크 id를 통해 지점 이름과 지역을 받아오는 기능 추가

---
+++ src/recoil/kioskIdAtom.js
@@ -0,0 +1,6 @@
+const { atom } = require("recoil");
+
+export const kioskIdAtom = atom({
+  key: "kioskIdAtom",
+  default: null,
+});
---
+++ src/recoil/kioskLocationAtom.js
@@ -0,0 +1,6 @@
+const { atom } = require("recoil");
+
+export const kioskLocationAtom = atom({
+  key: "kioskLocationAtom",
+  default: null,
+});
